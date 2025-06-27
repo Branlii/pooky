829ffa07d73c52c84d963ba53bcf82cd47701f1d
Branlii <guillot.raphael11@gmail.com>
2025-06-27 08:11:25 +0200

    feat: implement the database module

--- apps/cqrs-server/src/app/app.module.ts
+++ apps/cqrs-server/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { DatabaseServiceModule } from "@pooky/database-service";
 import { EventDispatcherKafkaModule } from "@pooky/event-dispatcher-kafka";
 import { EventStoreKurrentModule } from "@pooky/event-store-kurrent";
 import { AppController } from "./app.controller";
@@ -8,6 +9,7 @@ import { AppService } from "./app.service";
 	imports: [
 		EventDispatcherKafkaModule.register(),
 		EventStoreKurrentModule.register(),
+		DatabaseServiceModule.forRoot(),
 	],
 	controllers: [AppController],
 	providers: [AppService],
