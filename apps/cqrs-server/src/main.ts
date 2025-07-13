761100e9ae6e458832be27dbf548b4b193de3e6b
Branlii <guillot.raphael11@gmail.com>
2025-07-13 18:57:38 +0200

    feat: first swagger implementation

--- apps/cqrs-server/src/main.ts
+++ apps/cqrs-server/src/main.ts
@@ -3,17 +3,29 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+	const app = await NestFactory.create(AppModule);
+	const globalPrefix = "api";
+	app.setGlobalPrefix(globalPrefix);
+	const port = process.env.PORT || 3000;
+
+	const config = new DocumentBuilder()
+		.setTitle("Pooky API")
+		.setDescription("You can have access to all the endpoints of Pooky API")
+		.setVersion("1.0")
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("docs", app, documentFactory);
+
+	await app.listen(port);
+	Logger.log(
+		`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+	);
 }
 
 bootstrap();
--- libs/cqrs/queries/src/lib/controllers/modules.controller.ts
+++ libs/cqrs/queries/src/lib/controllers/modules.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from "@nestjs/common";
 import { QueryBus } from "@nestjs/cqrs";
+import { ApiOkResponse } from "@nestjs/swagger";
 import { GetAllModulesQuery } from "../definitions";
 
 @Controller("modules")
@@ -7,6 +8,9 @@ export class ModulesController {
 	constructor(private readonly queryBus: QueryBus) {}
 
 	@Get()
+	@ApiOkResponse({
+		description: "Returns all modules",
+	})
 	async getModules() {
 		return this.queryBus.execute(new GetAllModulesQuery());
 	}
