df5e79df113bbed524a5909ac66f44aa19220316
Branlii <guillot.raphael11@gmail.com>
2025-07-30 20:06:31 +0200

    feat: add nextjs web platform app

---
+++ apps/web-platform/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Index() {
+	/*
+	 * Replace the elements below with your own.
+	 *
+	 * Note: The corresponding styles are in the ./index.tailwind file.
+	 */
+	return <p>Hello world</p>;
+}
